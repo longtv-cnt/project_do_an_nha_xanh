52013078da97cd4d460819808a6db848741b48f2
taito1612 <57082579+taito1612@users.noreply.github.com>
2022-09-26 19:23:27 +0700

    frontend and filter

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\RealEsatateController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\khachhangController;
 use App\Http\Controllers\du_anController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
