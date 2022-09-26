bd5dca71db5da1d0f4d3b2f71ee7318d5774199d
taito1612 <57082579+taito1612@users.noreply.github.com>
2022-09-26 19:22:25 +0700

    frontend and filter

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RealEsatateController;
 use App\Http\Controllers\SearchController;
-use App\Http\Controllers\du_anController;
-//use App\Http\Controllers\HomeController;
 use App\Http\Controllers\khachhangController;
+use App\Http\Controllers\du_anController;
 
 /*
 |--------------------------------------------------------------------------
