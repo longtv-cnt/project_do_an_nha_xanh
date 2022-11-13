3396166f4e3a26dceb02feaf53e817cf728e563a
Trần Văn Long <longtv97.cnt@gmail.com>
2022-11-14 06:49:20 +0700

    bỏ

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,7 @@ class User extends Authenticatable
     public function roles(){
         return $this->belongsToMany('App\Models\Role','role_user','user_id','role_id');
     }
+    public function checkPermissionAccess($permissionCheck){
+
 }
+
